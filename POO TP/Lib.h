1563ad0ca5b2ffe2395f65565b0884bae046aa7f
RTSC14 <45045478+RTSC14@users.noreply.github.com>
2018-11-14 17:44:12 +0000

    v0.1
    beta

---
+++ POO TP/Lib.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <ctype.h>
+#include <cstdlib>
+#include <locale.h>
+#include <time.h>
+#include <fstream>
+#include <cmath>
+
+#include "Consola.h"
+
+using namespace std;
---
+++ POO TP/main.cpp
@@ -0,0 +1,40 @@
+﻿#include "Lib.h"
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+
+	bool flag = false;
+	int valor;
+
+	srand((unsigned int)time(NULL));
+	Consola::setScreenSize(32, 128);
+	Consola::setTextColor(Consola::VERDE);
+
+
+	while (flag == false) {
+		Consola::gotoxy(39, 1);
+		Consola::setTextSize(18, 18);
+		cout << "TRABALHO PRÁTICO - PROGRAMAÇĂO ORIENTADA A OBJECTOS";
+		Consola::gotoxy(50, 3);
+		cout << "1 - NOVO JOGO";
+		Consola::gotoxy(50, 4);
+		cout << "2 - CARREGAR JOGO";
+		Consola::gotoxy(50, 5);
+		cout << "3 - SAIR";
+
+		Consola::gotoxy(45, 7);
+		cout << "> ";
+		string s;
+		cin >> s;
+		istringstream iss(s);
+
+		if (iss >> valor) {
+			if (valor > 0 && valor < 4) {
+				flag = true;
+			}
+		}
+
+		Consola::clrscr();
+	}
+}
+
