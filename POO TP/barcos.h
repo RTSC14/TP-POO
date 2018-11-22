62872525679d7b6a139859083082370ac0b0a684
RTSC14 <45045478+RTSC14@users.noreply.github.com>
2018-11-22 19:02:38 +0000

    Alteracao Barcos

--- POO TP/barcos.h
+++ POO TP/barcos.h
@@ -1,8 +1,25 @@
 #pragma once
 class barcos
 {
+	int id;
+	char tipo;
+	int x, y;
+
+	
 public:
-	barcos();
+	barcos(int xx, int yy, char tipo);
+	
+	void setX(int xx);
+	int getX();
+
+	void setY(int yy);
+	int getY();
+
+	int getId();
+	char getTipo();
+
+
+
 	~barcos();
 };
 
